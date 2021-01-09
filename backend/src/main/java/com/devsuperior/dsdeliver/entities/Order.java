7d0ca9b33b1ba208c5184b886bf04448ba8e8e8c
unknown <rafaelduarte00@gmail.com>
2021-01-09 12:00:31 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -22,7 +22,7 @@ public class Order implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)	
-	private long id;
+	private Long id;
 	private String address;
 	private double latitude;
 	private double longitude;
@@ -38,7 +38,7 @@ public class Order implements Serializable {
 	public Order(){
 	}
 
-	public Order(long id, String address, double latitude, double longitude, Instant moment, OrderStatus status) {
+	public Order(Long id, String address, double latitude, double longitude, Instant moment, OrderStatus status) {
 		super();
 		this.id = id;
 		this.address = address;
@@ -48,11 +48,11 @@ public class Order implements Serializable {
 		this.status = status;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
